d81cbb499626ad1e3fdcd4d4c7cc23fdf8cf4e73
Yannick Formaggio <yannick.formaggio@gmail.com>
2019-06-20 16:24:00 -0700

    Chapter 2 done.

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -2,6 +2,8 @@
 
 void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) {
 	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("Sample driver Unload called\n"));
 }
 
 extern "C"
@@ -9,6 +11,15 @@ NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING Regi
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 	DriverObject->DriverUnload = SampleUnload;
+
+	KdPrint(("Sample driver successfully initialized\n"));
+
+	// Exercise chapter 2 - Output the Windows OS version: major, minor and build number
+	RTL_OSVERSIONINFOW info;
+	info.dwOSVersionInfoSize = sizeof(RTL_OSVERSIONINFOW);
+
+	RtlGetVersion(&info);
+	KdPrint(("%d.%d.%d\n", info.dwMajorVersion, info.dwMinorVersion, info.dwBuildNumber));
 
 	return STATUS_SUCCESS;
 }
